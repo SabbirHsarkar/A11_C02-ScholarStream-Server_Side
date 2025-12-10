68cc7a192434b37629fa5f66d2d483e245d62e50
SabbirHsarkar <sabbirhossainsarkar575@gmail.com>
2025-12-10 11:25:01 +0600

    Student role functionality added

--- index.js
+++ index.js
@@ -2,11 +2,55 @@ const express= require('express');
 const cors= require('cors');
 require('dotenv').config();
 
-const port =process.env.PORT || 5000;
+const port =process.env.PORT ;
 
 const app=express();
 app.use(cors());
 app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.URI;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   
+    await client.connect();
+
+    const database= client.db('scholarstream')
+    const userCollections = database.collection('user')
+
+    app.post('/users',async(req,res)=>{
+
+      const userInfo=req.body
+      userInfo.role="Student"
+      userInfo.createdAt = new Date();
+
+      const result= await userCollections.insertOne(userInfo)
+      res.send(result);
+
+    })
+
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/',(req,res)=>{
     res.send("Hello, Sabbir")
