a88c4b96a56a12a1e87b11e35c8848d04edd2af1
SabbirHsarkar <sabbirhossainsarkar575@gmail.com>
2025-12-10 13:07:09 +0600

    Role Query Added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express= require('express');
 const cors= require('cors');
 require('dotenv').config();
 
-const port =process.env.PORT ;
+const port =process.env.PORT || 5000 ;
 
 const app=express();
 app.use(cors());
@@ -41,6 +41,18 @@ async function run() {
 
     })
 
+
+    app.get('/users/role/:email',async(req,res)=>{
+      const{email} = req.params;
+
+
+  const query = { email: email };
+  const result = await userCollections.findOne(query);
+
+  console.log("User Found:", result);
+  res.send(result);
+      
+    })
     
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
